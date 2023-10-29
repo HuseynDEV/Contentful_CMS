36dcbb4ea9934023317a3adb3658b1a77d591b7f
Huseyn Alizade <huseyna@maestro.st>
2023-10-30 01:27:12 +0400

    changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+import Projects from "./Projects";
+
+const App = () => {
+  return (
+    <h1>
+      <Hero />
+      <Projects />
+    </h1>
+  );
+};
+export default App;
